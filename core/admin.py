efb1b3a71d0f6836c7fba089e3ac1d7db412029c
George Bobrov <avrelum@gmail.com>
2022-05-13 17:27:06 +0000

    Creating basic models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile, Message
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'status']
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['sender', 'recipient', 'message_text', 'sent']
--- core/models.py
+++ core/models.py
@@ -1,3 +1,32 @@
+from distutils.command.upload import upload
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    picture = models.ImageField(upload_to='media/avatars/', blank=True)
+    status = models.CharField(max_length=250, null=True)
+
+    def __str__(self):
+        return f'Profile for user {self.user.username}'
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, 
+                               on_delete=models.CASCADE,
+                               related_name='sender')
+    recipient = models.ForeignKey(User,
+                                  on_delete=models.CASCADE,
+                                  related_name='recipient')
+    message_text = models.TextField()
+    sent = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['sent']
+
+    def __str__(self):
+        return f'From: {self.sender}\nTo: {self.recipient}\nAt: {self.sent}'
