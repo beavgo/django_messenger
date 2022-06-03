965af6e46d0c4f78aa6949a7d5f1b0e8fad5587a
George Bobrov <avrelum@gmail.com>
2022-06-03 19:08:14 +0000

    Added first frontend step

--- core/views.py
+++ core/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Chat
+
+
+@login_required
+def chats_list(request):
+    chats = Chat.objects.all()
+    return render(request, 'dashboard/chats_list.html', {'chats': chats})
--- messenger/urls.py
+++ messenger/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    path('', views.chats_list, name='chats'),
     path('admin/', admin.site.urls),
 ]
